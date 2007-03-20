fb2d158cbbd5ad6e7785e5f1c07d9e09a7f861f8
davidrodriguespinheiro <davidrodriguespinheiro@796ed23e-232c-0410-9067-1d6a512339f2>
2007-03-20 22:42:25 +0000

    Incluida licenca GPL nos fontes.

--- trunk/src/WaitDieException.java
+++ trunk/src/WaitDieException.java
@@ -1,3 +1,23 @@
+/*
+Simulador 2V2PL Simulator - Simula o escalonamento de operacoes de escrita, leitura
+e commit em uma ou varias transacoes em um banco de dados.
+
+Copyright (C) 2006 David Rodrigues Pinheiro - davidrodriguespinheiro at gmail dot com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 public class WaitDieException extends Exception {
 
